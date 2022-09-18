e7a4623b4bd7bc84fed86af5e7b9c2e559265564
Yana Vitkovskay <vitkovskaya.yana@gmail.com>
2022-09-18 20:34:52 +0300

    Add tests

---
+++ src/test/java/BonusServiceTest.java
@@ -0,0 +1,36 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+public class BonusServiceTest {
+
+    @org.junit.jupiter.api.Test
+    void shouldCalculateForUnRegisteredAndUnderLimit() {
+        BonusService service = new BonusService();
+
+        // подготавливаем данные:
+        long amount = 1000;
+        boolean registered = false;
+        long expected = 10;
+
+        // вызываем целевой метод:
+        long actual = service.calculate(amount, registered);
+
+        // производим проверку (сравниваем ожидаемый и фактический):
+        assertEquals(expected, actual);
+    }
+
+    @org.junit.jupiter.api.Test
+    void shouldCalculateForRegisteredAndOverLimit() {
+        BonusService service = new BonusService();
+
+        // подготавливаем данные:
+        long amount = 0;
+        boolean registered = true;
+        long expected = 0;
+
+        // вызываем целевой метод:
+        long actual = service.calculate(amount, registered);
+
+        // производим проверку (сравниваем ожидаемый и фактический):
+        assertEquals(expected, actual);
+    }
+}
